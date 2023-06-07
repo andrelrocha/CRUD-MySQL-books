29fce00d907437aa8ed9694d2a136ebc1f3dd819
andrelrocha <andrerocha0911@gmail.com>
2023-06-07 16:50:14 -0300

    refactor: refatorando o código separando a inicialização do pool de conexões

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { engine } from 'express-handlebars';
-import mysql from 'mysql';
+import pool from "./db/connection.js";
 
 const app = express();
 
@@ -17,19 +17,8 @@ app.set("view engine", "handlebars")
 
 app.use(express.static('public'))
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'nodemysql'
-})
+app.listen(3000, () => console.log("SERVER UP!"))
 
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('Database connected!');
-
-  app.listen(3000, () => console.log("SERVER UP!"))
-});
 
 
 
@@ -46,7 +35,7 @@ app.post("/books/insertbook", (req, res) => {
   const sql = `INSERT INTO books (title, pageqty) VALUES ('${title}','${pageqty}')`
 
   //efetivamente cria a query
-  connection.query(sql, (err) => {
+  pool.query(sql, (err) => {
     if (err) throw err;
 
     res.redirect("/books")
@@ -57,7 +46,7 @@ app.post("/books/insertbook", (req, res) => {
 app.get("/books", (req, res) => {
   const sql = "SELECT * FROM books"
 
-  connection.query(sql, (err, data) => {
+  pool.query(sql, (err, data) => {
     if (err) throw err;
 
     const books = data
@@ -72,7 +61,7 @@ app.get("/books/:id", (req, res) => {
 
   const sql = `SELECT * FROM books WHERE id = ${id}`
 
-  connection.query(sql, (err, data) => {
+  pool.query(sql, (err, data) => {
     if (err) throw err;
 
     const book = data[0]
@@ -87,7 +76,7 @@ app.get("/books/edit/:id", (req, res) => {
 
   const sql = `SELECT * FROM books WHERE id = ${id}`
 
-  connection.query(sql, (err, data) => {
+  pool.query(sql, (err, data) => {
     if (err) throw err;
 
     const book = data[0]
@@ -104,7 +93,7 @@ app.post('/books/updatebook', (req, res) => {
 
   const sql = `UPDATE books SET title = '${title}', pageqty = '${pageqty}' WHERE id = ${id}`
 
-  connection.query(sql, (err) => {
+  pool.query(sql, (err) => {
     if (err) throw err;
 
     res.redirect('/books')
@@ -113,13 +102,13 @@ app.post('/books/updatebook', (req, res) => {
 
 // DELETE
 app.post('/books/remove/:id', (req, res) => {
-    const id = req.params.id
-  
-    const sql = `DELETE FROM books WHERE id = ${id}`
-  
-    connection.query(sql, (err) => {
-      if (err) throw err;
-  
-      res.redirect('/books')
-    })
+  const id = req.params.id
+
+  const sql = `DELETE FROM books WHERE id = ${id}`
+
+  pool.query(sql, (err) => {
+    if (err) throw err;
+
+    res.redirect('/books')
+  })
 })
